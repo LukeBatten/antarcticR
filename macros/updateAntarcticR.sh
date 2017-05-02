6cc1d4dbd4ade04ebd05b0ee89fcf94198593260
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-05-02 16:28:00 +0100

    further documentation, some pushing changes

--- macros/updateAntarcticR.sh
+++ macros/updateAntarcticR.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# remove existing pdf
+rm ../../antarcticR.pdf
+
 # update all packages
 Rscript updateCustomPackages.R
+cd ../..
 R CMD Rd2pdf antarcticR
-cp ../../antarcticR.pdf 
+cp antarcticR.pdf ./antarcticR
 
-git add ../*
+cd ./antarcticR
+echo "Git..."
+git add *
 git 
 git commit
 git push origin master
