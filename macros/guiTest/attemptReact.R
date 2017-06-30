cb2eb9f8f99c50132b8e59ed882295a6240a876c
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-06-30 00:20:27 -0700

    Can't get dynamic aggregating raster. Works for direct variables though...

--- macros/guiTest/attemptReact.R
+++ macros/guiTest/attemptReact.R
@@ -4,10 +4,6 @@ library("shiny")
 library("antarcticR")
 library("Cairo")
 
-########## TO DO##
-## Radio buttons for type of bedmap 2 data
-#################
-
 ui <- fluidPage(
     
     headerPanel(HTML(paste("antarcticR",tags$sup("online")))),
@@ -87,79 +83,13 @@ shinyServer <- function(input, output) {
     
     output$antarcticCanvas <- renderPlot({
         ggplot()+
-            geom_point(data = antFrame, aes(x = easting, y = northing), size=2, color="red") +
+            geom_point(data = antFrame, aes(x = easting, y = northing), size=input$sliderRes, color="red") +
             geom_tile(data=bmdf,aes(bbb,ccc,fill=varFillBBB)) +
-            geom_point(data = antFrame, aes(x = easting, y = northing), size=2, color="red") +
-            guides(fill=guide_legend(title="ice thickness")) +
-            coord_cartesian(xlim = ranges$easting, ylim = ranges$northing, expand = FALSE) ## Needed for zooming
+            geom_point(data = antFrame, aes(x = easting, y = northing), size=input$sliderRes, color="red") +
+          guides(fill=guide_legend(title="ice thickness"))
 
     })
 
-    output$text1 <- renderText({ 
-        paste("You have selected", input$sliderRes)
-    })
-    
-
-######
-
-###### ZOOM in
-
-    ranges <- reactiveValues(easting = NULL, northing = NULL)
-    
-    observeEvent(input$antarcticCanvas_dblclick, {
-        brush <- input$antarcticCanvas_brush
-        if (!is.null(brush)) {
-            ranges$easting <- c(brush$xmin, brush$xmax)
-            ranges$northing <- c(brush$ymin, brush$ymax)
-
-        } else {
-            ranges$easting <- NULL
-            ranges$northing <- NULL
-        }
-    })
-    
-###### Hover
-
-    output$hover_info <- renderUI({        
-        hover <- input$plot_hover
-
-        point <- nearPoints(antFrame, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
-
-        if(1)
-        {
-            
-            if (nrow(point) == 0) return(NULL)
-            
-            left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
-            top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
-            
-                                        # calculate distance from left and bottom side of the picture in pixels
-            left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
-            top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
-            
-                                        # create style property for tooltip
-                                        # background color is set so tooltip is a bit transparent
-                                        # z-index is set so we are sure are tooltip will be on top
-            style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
-                            "left:", left_px + 2, "px; top:", top_px + 2, "px;")
-            
-                                        # actual tooltip created as wellPanel
-            wellPanel(
-                style = style,
-                p(HTML(paste0("<b> Name: </b>", point$name, "<br/>",
-                              "<b> Primary operator: </b>", point$primaryOperator, "<br/>",
-                              "<b> Established: </b>", point$est, "<br/>",
-                              "<b> Facility Type: </b>", point$facType, "<br/>",
-                              "<b> Seasonality: </b>", point$seasonality, "<br/>",
-                              "<b> Altitude: </b>", point$alt, "<br/>",
-                              "<b> Longitude: </b>", point$long, "<br/>",
-                              "<b> Latitude: </b>", point$lat, "<br/>"))))
-        }
-    })
-
-    
-    
-    
 } ## server end
 
 print("Processed code")
